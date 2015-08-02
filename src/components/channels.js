fdebe2200b87a4a6bf18d06156f870b2bdd32b55
johnamiahford <johnamiahford@gmail.com>
2015-08-02 14:38:50 -0400

    Add react-tooltip

--- src/components/channels.js
+++ src/components/channels.js
@@ -1,13 +1,22 @@
 'use strict';
 
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
 
 class Channels extends React.Component {
 
   render() {
     return (
-      <div className="Channels u-clearfix">
-        <h3>Channels</h3>
+      <div className="Channels">
+        <div className="u-floatRight">
+          <i className="icon icon_plus_circle u-cursor" data-tip="Create new channel" data-position="{'top': 3, 'right': 10}"></i>
+        </div>
+        <div className="grid">
+          <div className="grid-cell">
+            <h3 className="Channels-title u-cursor" data-tip="Browse all channels">Channels</h3>
+          </div>
+        </div>
+        <ReactTooltip effect="solid" />
       </div>
     );
   }
